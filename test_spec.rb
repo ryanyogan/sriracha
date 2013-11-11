5b64afeb9e4755d8f368a3b1df5d67091b4b167b
Ryan Yogan <ryanyogan@gmail.com>
2013-11-11 02:57:43 -0600

    Basic test passing for description and it

--- test_spec.rb
+++ test_spec.rb
@@ -4,14 +4,18 @@ require_relative 'spec'
 class TestDescribe < MiniTest::Unit::TestCase
   def test_that_it_can_pass
     describe "some thing" do
+      it "has some property" do
 
+      end
     end
   end
 
   def test_that_it_can_fail
-    describe "some failing thing" do
-      it "fails" do
-        raise IndexError
+    assert_raises(IndexError) do
+      describe "some failing thing" do
+        it "fails" do
+          raise IndexError
+        end
       end
     end
   end
